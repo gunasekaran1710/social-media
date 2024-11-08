87a0e34f4299f3c5dce2e9e50341d01cb3fed781
gunasekaran1710 <guna171003@gmail.com>
2024-11-08 17:53:57 +0530

    initial commit

---
+++ app/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from .config import settings
+from fastapi.middleware.cors import CORSMiddleware
+from . import models
+from .database import engine
+from .router import post,user,auth,vote
+app = FastAPI()
+origins=["*"]
+app.add_middleware(CORSMiddleware,allow_origins=origins,allow_credentials=True,allow_methods=["*"],
+                   allow_headers=["*"])
+app.include_router(post.router)
+app.include_router(user.router)
+app.include_router(auth.router)
+app.include_router(vote.router)
+
+@app.get('/')
+def root():
+    return{"message":"hello world"}
+
+
+
+
+
+
